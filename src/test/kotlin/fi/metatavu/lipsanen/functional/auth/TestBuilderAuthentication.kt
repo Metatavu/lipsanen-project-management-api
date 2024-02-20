6244aa5ea8c5de99f62ba2c6fae41b3f7662295c
Katja Danilova <katja.danilova@protonmail.com>
2024-02-20 10:50:30 +0200

    init commit

---
+++ src/test/kotlin/fi/metatavu/lipsanen/functional/auth/TestBuilderAuthentication.kt
@@ -0,0 +1,29 @@
+package fi.metatavu.lipsanen.functional.auth
+
+import fi.metatavu.jaxrs.test.functional.builder.auth.AccessTokenProvider
+import fi.metatavu.jaxrs.test.functional.builder.auth.AccessTokenTestBuilderAuthentication
+import fi.metatavu.lipsanen.functional.TestBuilder
+import fi.metatavu.lipsanen.functional.settings.ApiTestSettings
+import fi.metatavu.lipsanen.test.client.infrastructure.ApiClient
+
+/**
+ * Test builder authentication
+ *
+ * @author Jari Nykänen
+ * @author Antti Leppä
+ *
+ * @param testBuilder test builder instance
+ * @param accessTokenProvider access token provider
+ */
+class TestBuilderAuthentication(
+    private val testBuilder: TestBuilder,
+    private val accessTokenProvider: AccessTokenProvider
+) : AccessTokenTestBuilderAuthentication<ApiClient>(testBuilder, accessTokenProvider) {
+
+    override fun createClient(authProvider: AccessTokenProvider): ApiClient {
+        val result = ApiClient(ApiTestSettings.apiBasePath)
+        ApiClient.accessToken = authProvider.accessToken
+        return result
+    }
+
+}
---
+++ src/test/kotlin/fi/metatavu/lipsanen/functional/settings/DefaultTestProfile.kt
@@ -0,0 +1,15 @@
+package fi.metatavu.lipsanen.functional.settings
+
+import io.quarkus.test.junit.QuarkusTestProfile
+
+/**
+ * Default test profile
+ */
+class DefaultTestProfile: QuarkusTestProfile {
+
+    override fun getConfigOverrides(): MutableMap<String, String> {
+        val config: MutableMap<String, String> = HashMap()
+        config["vp.env"] = "TEST"
+        return config
+    }
+}
