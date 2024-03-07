26dd467b7f919edd09736fc9f7c318df89bfafa5
Katja Danilova <katja.danilova@protonmail.com>
2024-03-07 10:54:42 +0200

    reflection annotations

--- src/main/kotlin/fi/metatavu/lipsanen/tocoman/Projects.kt
+++ src/main/kotlin/fi/metatavu/lipsanen/tocoman/Projects.kt
@@ -1,11 +1,13 @@
 package fi.metatavu.lipsanen.tocoman
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.quarkus.runtime.annotations.RegisterForReflection
 import java.util.*
 
 /**
  * POJO for Tocoman XML projects
  */
+@RegisterForReflection
 class Project {
     @JsonProperty("ID")
     var id: Int = 0
@@ -16,7 +18,7 @@ class Project {
     @JsonProperty("Changed")
     var changed: Date? = null
 }
-
+@RegisterForReflection
 class Projects {
     @JsonProperty("Project")
     var project: Project? = null
