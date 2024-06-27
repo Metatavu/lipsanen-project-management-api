3b612f954ff3b9be5340ed61b3868a9b40266109
Katja Danilova <katja.danilova@protonmail.com>
2024-06-27 12:48:36 +0300

    include keycloak search

--- src/main/kotlin/fi/metatavu/lipsanen/rest/MetadataTranslator.kt
+++ src/main/kotlin/fi/metatavu/lipsanen/rest/MetadataTranslator.kt
@@ -1,7 +1,9 @@
 package fi.metatavu.lipsanen.rest
 
 import fi.metatavu.lipsanen.persistence.Metadata
+import fi.metatavu.lipsanen.users.UserController
 import jakarta.enterprise.context.ApplicationScoped
+import jakarta.inject.Inject
 
 /**
  * Translator for metadata
@@ -9,10 +11,20 @@ import jakarta.enterprise.context.ApplicationScoped
 @ApplicationScoped
 class MetadataTranslator :
     AbstractTranslator<Metadata, fi.metatavu.lipsanen.api.model.Metadata>() {
+
+    @Inject
+    lateinit var userController: UserController
+
     override suspend fun translate(entity: Metadata): fi.metatavu.lipsanen.api.model.Metadata {
+        val creatorId = userController.findUserByKeycloakId(entity.creatorId)?.id
+        val modifierId = if (entity.creatorId == entity.lastModifierId) {
+            creatorId
+        } else {
+            userController.findUserByKeycloakId(entity.lastModifierId)?.id
+        }
         return fi.metatavu.lipsanen.api.model.Metadata(
-            creatorId = entity.creatorId,
-            lastModifierId = entity.lastModifierId,
+            creatorId = creatorId,
+            lastModifierId = modifierId,
             createdAt = entity.createdAt,
             modifiedAt = entity.modifiedAt
         )
