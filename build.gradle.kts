69425feb751ab2403d11efd0ce7e22837c7f3e31
Lauri Lukkarinen <44664737+lauriLukkarinen@users.noreply.github.com>
2024-08-21 23:26:52 +0300

    Added generate task to Gradle
    The task combines all generator tasks for convenience

--- build.gradle.kts
+++ build.gradle.kts
@@ -103,7 +103,7 @@ sourceSets["test"].java {
 val generateApiSpec = tasks.register("generateApiSpec", GenerateTask::class){
     setProperty("generatorName", "kotlin-server")
     setProperty("inputSpec",  "$rootDir/lipsanen-project-management-spec/swagger.yaml")
-    setProperty("outputDir", "$buildDir/generated/api-spec")
+    setProperty("outputDir", "${layout.buildDirectory}/generated/api-spec")
     setProperty("apiPackage", "${project.group}.api.spec")
     setProperty("invokerPackage", "${project.group}.api.invoker")
     setProperty("modelPackage", "${project.group}.api.model")
@@ -124,7 +124,7 @@ val generateApiClient = tasks.register("generateApiClient", GenerateTask::class)
     setProperty("generatorName", "kotlin")
     setProperty("library", "jvm-okhttp3")
     setProperty("inputSpec",  "$rootDir/lipsanen-project-management-spec/swagger.yaml")
-    setProperty("outputDir", "$buildDir/generated/api-client")
+    setProperty("outputDir", "${layout.buildDirectory}/generated/api-client")
     setProperty("packageName", "${project.group}.test.client")
     setProperty("validateSpec", false)
 
@@ -138,7 +138,7 @@ val generateKeycloakClient = tasks.register("generateKeycloakAdminClient",Genera
     setProperty("generatorName", "kotlin")
     setProperty("library", "jvm-vertx")
     setProperty("inputSpec",  "$rootDir/src/main/resources/kc-admin.json")
-    setProperty("outputDir", "$buildDir/generated/keycloak-admin-client")
+    setProperty("outputDir", "${layout.buildDirectory}/generated/keycloak-admin-client")
     setProperty("packageName", "fi.metatavu.keycloak.adminclient")
 
     this.configOptions.put("useCoroutines", "true")
@@ -148,6 +148,17 @@ val generateKeycloakClient = tasks.register("generateKeycloakAdminClient",Genera
     this.configOptions.put("enumPropertyNaming", "UPPERCASE")
     this.configOptions.put("useCoroutines", "true")
     this.configOptions.put("additionalModelTypeAnnotations", "@io.quarkus.runtime.annotations.RegisterForReflection")
+}
+
+tasks.register("generate") {
+    this.doFirst {
+        file("$rootDir/build/generated").deleteRecursively()
+    }
+    this.finalizedBy(
+        generateApiSpec,
+        generateApiClient,
+        generateKeycloakClient
+    )
 }
 
 tasks.named("compileKotlin") {
